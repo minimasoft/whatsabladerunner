e223b060f850c6d76de4edc94c45b71d4a0fa6a2
Esteban Torre <minimasoft@proton.me>
2025-12-10 14:16:09 -0300

    working context && message recv

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sort"
 	"syscall"
 	"time"
 
 	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waHistorySync"
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
@@ -21,7 +23,47 @@ import (
 func eventHandler(evt interface{}) {
 	switch v := evt.(type) {
 	case *events.Message:
-		fmt.Println("Received a message!", v.Message.GetConversation())
+		fmt.Println("------------------------------------------------")
+		fmt.Printf("Received a message!\n")
+		fmt.Printf("ID: %s\n", v.Info.ID)
+		fmt.Printf("Time: %s\n", v.Info.Timestamp)
+		fmt.Printf("Sender: %s (PushName: %s)\n", v.Info.Sender, v.Info.PushName)
+		fmt.Printf("Chat: %s\n", v.Info.Chat)
+		if v.Message != nil {
+			fmt.Printf("Content: %+v\n", v.Message)
+		}
+		fmt.Println("------------------------------------------------")
+	case *events.HistorySync:
+		id := v.Data.GetSyncType()
+		if id == waHistorySync.HistorySync_FULL || id == waHistorySync.HistorySync_RECENT {
+			fmt.Printf("Received History Sync (Type: %s)\n", id)
+
+			conversations := v.Data.GetConversations()
+			// Sort by LastMsgTimestamp descending
+			sort.Slice(conversations, func(i, j int) bool {
+				return conversations[i].GetLastMsgTimestamp() > conversations[j].GetLastMsgTimestamp()
+			})
+
+			for _, conv := range conversations {
+				fmt.Printf("Conversation: %s (%s)\n", conv.GetID(), conv.GetName())
+				if conv.GetUnreadCount() > 0 {
+					fmt.Printf("  Unread: %d\n", conv.GetUnreadCount())
+				}
+				if conv.GetIsParentGroup() || conv.GetIsDefaultSubgroup() {
+					fmt.Println("  [Group/Community]")
+				}
+
+				// List participants if available (HistorySync might not always have full participant list for all convs, but let's check)
+				// Note: Conversation proto in HistorySync has 'participant' field
+				participants := conv.GetParticipant()
+				if len(participants) > 0 {
+					fmt.Println("  Participants:")
+					for _, p := range participants {
+						fmt.Printf("    - %s\n", p.GetUserJID())
+					}
+				}
+			}
+		}
 	}
 }
 
@@ -30,7 +72,7 @@ func main() {
 	// | NOTE: You must also import the appropriate DB connector, e.g. github.com/mattn/go-sqlite3 for SQLite |
 	// |------------------------------------------------------------------------------------------------------|
 
-	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	dbLog := waLog.Stdout("Database", "INFO", true)
 	ctx := context.Background()
 	container, err := sqlstore.New(ctx, "sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
 	if err != nil {
@@ -41,7 +83,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	clientLog := waLog.Stdout("Client", "INFO", true)
 	client := whatsmeow.NewClient(deviceStore, clientLog)
 	client.AddEventHandler(eventHandler)
 
@@ -72,10 +114,10 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	// List conversations after a short delay to allow connection/sync
+	// List stored conversations after a short delay to allow connection/store readiness
 	go func() {
-		time.Sleep(5 * time.Second)
-		listConversations(client)
+		time.Sleep(3 * time.Second)
+		listStoredConversations(client)
 	}()
 
 	<-c
@@ -83,8 +125,8 @@ func main() {
 	client.Disconnect()
 }
 
-func listConversations(client *whatsmeow.Client) {
-	fmt.Println("Fetching conversations...")
+func listStoredConversations(client *whatsmeow.Client) {
+	fmt.Println("Fetching stored conversations...")
 
 	// List Groups
 	groups, err := client.GetJoinedGroups(context.Background())
@@ -99,7 +141,7 @@ func listConversations(client *whatsmeow.Client) {
 				fmt.Printf("    - %s\n", p.JID)
 			}
 
-			// Check chat settings (Unread count not directly available in LocalChatSettings in this version)
+			// Check chat settings
 			if client.Store.ChatSettings != nil {
 				settings, err := client.Store.ChatSettings.GetChatSettings(context.Background(), g.JID)
 				if err == nil {
